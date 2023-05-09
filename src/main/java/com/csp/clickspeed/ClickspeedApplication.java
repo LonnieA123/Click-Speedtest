5a742fe7e062e3051bce6c6cb0e47678e1910c91
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2023-05-09 18:08:48 -0500

    inital commit

---
+++ src/main/java/com/csp/clickspeed/ClickspeedApplication.java
@@ -0,0 +1,13 @@
+package com.csp.clickspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickspeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickspeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csp/clickspeed/ClickspeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.clickspeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickspeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
