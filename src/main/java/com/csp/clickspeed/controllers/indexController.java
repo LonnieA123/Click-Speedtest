07ddd5d8bb0914285e5880e80b241d3a612e59a9
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2023-05-10 15:21:05 -0500

    set up and index page

---
+++ src/main/java/com/csp/clickspeed/controllers/indexController.java
@@ -0,0 +1,14 @@
+package com.csp.clickspeed.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
